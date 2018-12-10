2e9254f330619ddc528b144c064b95f8f8a7f90f
Jesper Pedersen <jesper@yepz.dk>
2018-12-10 13:18:04 +0100

    ER-252: Align text to bottom of row

--- templates/paragraphs-item--campaign-material.tpl.php
+++ templates/paragraphs-item--campaign-material.tpl.php
@@ -6,6 +6,8 @@
 ?>
 <div class="campaign-material">
     <?php print render($variables['cover']) ?>
-    <h3><?php print render($variables['title']) ?></h3>
-    <?php print render($variables['author']) ?>
+    <div class="campaign-material-info">
+      <h3><?php print render($variables['title']) ?></h3>
+      <?php print render($variables['author']) ?>
+    </div>
 </div>
